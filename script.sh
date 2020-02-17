c734838c3d96bbda48d1705ca4aed27c20a30502
Prabhu Pant <prabhupant09@gmail.com>
2020-02-17 13:01:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,22 @@
-read -p "Enter password : " password
-echo "$password" | sudo -S su
+#!/bin/bash
+
+sudo su
+
+# updating apt and apt-get
+apt-get update
+apt update
+
+# Languages
+apt-get install python3
 
 # Installating tools for Linux
-sudo apt-get install linux-tools-common
-sudo apt-get install htop
+apt-get install linux-tools-common -y
+apt-get install htop -y
+
+# Installing IDEs and Editors
+apt-get install vim
+snap install --classic code
+add-apt-repository ppa:webupd8team/atom
+apt-get update
+apt-get install atom
+apt-get install awscli -y
